e0489e82e53fc8b576762dcc552b2f58d49c6be2
Izabela Awerczuk <izunqua@gmail.com>
2024-03-10 12:34:08 +0100

    add FilmModel, FilmController, FilmManager

---
+++ Controllers/FilmController.cs
@@ -0,0 +1,27 @@
+﻿using FilmDB.Models;
+using FilmDB.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    public class FilmController : Controller
+    {
+        private readonly FilmManager _manager;
+        public FilmController(FilmManager manager)
+        {
+            _manager = manager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var film = new FilmModel()
+            {
+                Title = "One day",
+                Year = 2011
+            };
+
+            await _manager.AddFilmAsync(film);
+
+            return View();
+        }
+    }
+}
